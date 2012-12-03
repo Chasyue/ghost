5efaa9a805fb28f47d483fa5645a92d04e29dc79
wenming <tang@babyfamily.com>
2012-12-03 18:06:11 +0800

    bug fixed

--- src/Ghost/PostBundle/EntityManager/PostManager.php
+++ src/Ghost/PostBundle/EntityManager/PostManager.php
@@ -69,6 +69,7 @@ class PostManager extends BasePostManager
             ->join('p.user', 'u')
             ->where('t.id = :topic')
             ->andWhere('p.isDeleted = 0')
+            ->orderBy('p.created', 'desc')
             ->setParameter('topic', $topic->getId());
 
         $query = $qb->getQuery();
